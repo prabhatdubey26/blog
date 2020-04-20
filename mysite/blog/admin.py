6221891de555d6820c7b1abae7a53166308c2253
prabhat <prabhatdubeyakb@gmail.com>
2020-04-21 03:02:52 +0530

    21

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, BlogComment
 # Register your models here.
 
 
 class PostDisplay(admin.ModelAdmin):
-    list_display = ['sno','title','content','author', 'slug','timestamp']
-    list_filter = ['sno', 'title','author']
+    list_display = ['title','content','author', 'slug','timestamp']
+    list_filter = [ 'title','author']
+
+class CommentDisplay(admin.ModelAdmin):
+    list_display = ['comment','user','post', 'parent','timestamp']
+    list_filter = [ 'user','post']
 
 admin.site.register(Post,PostDisplay)
+admin.site.register(BlogComment,CommentDisplay)
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+# API to post a comment
+path('postComment',views.postComment, name="postComment"),
+
 path('',views.blogHome , name="blogHome" ),
 path('<str:slug>',views.blogPost , name="blogPost" ),
+
 
 
 ]
