3ab181f62f305d91eef6e91283b5500b38aa3855
prabhat <prabhatdubeyakb@gmail.com>
2020-04-28 02:16:09 +0530

    sdd

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,HttpResponse, redirect
 from blog.models import Post, BlogComment
 from django.contrib import messages
+from blog.templatetags import extras
 
 # Create your views here.
 def blogHome(request):
@@ -10,8 +11,16 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(post=post)
-    context = {'post':post, 'comments':comments, 'user':request.user}
+    comments = BlogComment.objects.filter(post=post, parent=None)
+    replies = BlogComment.objects.filter(post=post).exclude(parent=None)
+    replyDict = {}
+    for reply in replies:
+        if reply.parent.sno not in replyDict.keys():
+            replyDict[reply.parent.sno] = [reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
+    print(replyDict)
+    context = {'post':post, 'comments':comments, 'user':request.user,'replyDict':replyDict,'replies':replies}
     return render(request,'blog/blogPost.html', context)
 
 
@@ -26,9 +35,9 @@ def postComment(request):
         if parent == '':
             comments = BlogComment(comment=comment,user=user,post=post)
             comments.save()
-            messages.success(request, "Your comment has been posted successfully")
+            messages.success(request, "Your comment has been posted successfully.")
         else:
             comments = BlogComment(comment=comment,user=user,post=post,parent=parent)
             comments.save()
-            messages.success(request, "Your comment has been posted successfully")
+            messages.success(request, "Your reply has been posted successfully.")
     return redirect(f"/blog/{post.slug}")
